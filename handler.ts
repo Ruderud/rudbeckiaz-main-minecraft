1468906ec212fd74efb10cc9578f1c72474587e0
정경훈 <ruderud0055@gmail.com>
2023-09-05 14:56:20 +0900

    config: tsc init

--- handler.ts
+++ handler.ts
@@ -1,6 +1,7 @@
+import { Handler } from 'aws-lambda';
 import { roomHandler } from './src';
 
-const entryHandler = async (event) => {
+const entryHandler: Handler = async (event) => {
   console.log('entryHandler', event);
   return {
     status: 200,
